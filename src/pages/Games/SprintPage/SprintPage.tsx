99a07022214b370db09f2c71ed8e20aa76dbaa56
m208 <avcvet@yandex.ru>
2022-08-27 16:40:33 +0300

    style: change styles

--- src/pages/Games/SprintPage/SprintPage.tsx
+++ src/pages/Games/SprintPage/SprintPage.tsx
@@ -1,4 +1,3 @@
-import { useSelector } from 'react-redux';
 import { useSearchParams } from 'react-router-dom';
 
 import { useEffect, useRef, useState } from 'react';
@@ -7,10 +6,9 @@ import './SprintPage.pcss';
 
 import { IGameResults, SprintBody } from './SprintBody/SprintBody';
 
+import { GameDescription } from '@/components/games/GameDescription/GameDescription';
+import { GameDifficulty } from '@/components/games/GameDifficulty/GameDifficulty';
 import { GameResults } from '@/components/games/GameResults/GameResults';
-import { Button } from '@/components/ui/Button/Button';
-import { Dropdown, OptionsType } from '@/components/ui/DropDown/DropDown';
-import type { RootState } from '@/store/store';
 
 export const SprintPage = (): JSX.Element => {
   const [level, setLevel] = useState(1);
@@ -26,13 +24,7 @@ export const SprintPage = (): JSX.Element => {
   const page = useRef('');
   const group = useRef('');
 
-  const authState = useSelector((state: RootState) => state.authentication);
-  const { isLoggedIn } = authState;
-  const levelsCount = isLoggedIn? 7 : 6;
-
-  const dropDownArray: OptionsType[] = Array(levelsCount).fill(1).map((el, i)=> ({ label: 'Уровень ', value: `${+i+1}` }));
-  const dropDownChanged = (value: string) => {setLevel(+value);
-  };
+  const dropDownChanged = (value: string) => {setLevel(+value - 1);  };
 
   const gameIsOver = (results: IGameResults) => {
     setGameEnded(true);
@@ -64,25 +56,22 @@ export const SprintPage = (): JSX.Element => {
   }, [firstRun, searchParams]);
 
   return (
-
     <div className="game">
-      <h2 className="game_heading">Мини игра &quot;Спринт&quot;</h2>
-      <p className="game_subheading">
-            В течение одной минуты отвечай соответствует ли слово предложенному переводу.
-        <br/>
-            Можно кликать по кнопкам, или нажимать клавиши стрелки  &#8678;&nbsp;&#8680;
-      </p>
 
       {!gameStarted && !gameEnded &&
-      <div className="game_start_buttons">
-        <Dropdown
-          options={dropDownArray}
-          name="Сложность"
-          initial="1"
-          onChange={dropDownChanged}
-        />
-        <Button buttonType='primary' text='Начать' onClick={()=>setgameStarted(true)} />
-      </div>
+      <GameDescription
+        name='Спринт'
+        description={[
+          'В течение одной минуты отвечай соответствует ли слово предложенному переводу.',
+          'Можно кликать по кнопкам, или нажимать клавиши стрелки',
+        ]}
+      />
+      }
+
+      {!gameStarted && !gameEnded &&
+      <GameDifficulty
+        onChangeDiff={dropDownChanged}
+        onStart={()=>setgameStarted(true)}/>
       }
 
       {gameStarted &&
