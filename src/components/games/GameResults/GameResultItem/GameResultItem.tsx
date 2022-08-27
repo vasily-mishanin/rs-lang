8ab4f9f06c8c50b667fcf508314847707626a76f
m208 <avcvet@yandex.ru>
2022-08-27 15:52:37 +0300

    refactor: styling game results component

---
+++ src/components/games/GameResults/GameResultItem/GameResultItem.tsx
@@ -0,0 +1,34 @@
+import classNames from 'classnames';
+
+import { PlayAudio } from '@/components/PlayAudio/PlayAudio';
+import { FILESTORAGE_URL } from '@/model/constants';
+import { ISprintWord } from '@/pages/Games/SprintPage/SprintBody/SprintBody';
+import './GameResultItem.pcss';
+
+export interface GameResultItemProps {
+  item: ISprintWord;
+  isOdd: boolean;
+}
+export const GameResultItem = ({ item, isOdd }: GameResultItemProps): JSX.Element => (
+  <div
+    className={
+      classNames('results_item', isOdd && 'result_odd')
+    }
+  >
+    <div className="results_cell result_audio">
+      <PlayAudio
+        source={FILESTORAGE_URL + item.audio}
+        type='single-button'
+      />
+    </div>
+    <div
+      className="results_cell results_name">
+      <span>{item.word}</span>
+    </div>
+    <div className="results_cell results_name">
+      <span>{item.wordTranslate}</span>
+    </div>
+
+  </div>
+
+);
--- src/components/games/GameResults/GameResults.tsx
+++ src/components/games/GameResults/GameResults.tsx
@@ -1,40 +1,50 @@
 import classNames from 'classnames';
 
+import { GameResultItem } from './GameResultItem/GameResultItem';
+
 import { IGameResults } from '../../../pages/Games/SprintPage/SprintBody/SprintBody';
 import './GameResults.pcss';
 
 export const GameResults =
-(  { correctAnswers,  wrongAnswers, score }: IGameResults): JSX.Element => {
+  ({ correctAnswers, wrongAnswers, score }: IGameResults): JSX.Element => {
 
-  const getResultDesision = () => {
-    if (wrongAnswers.length === 0) return 'Отличный результат!';
-    if (correctAnswers.length > wrongAnswers.length) return 'Хороший результат, продолжай учиться!';
-    return 'Продолжай учиться, в следующий раз получится';
-  };
+    const getResultDesision = () => {
+      if (correctAnswers.length === 0) return '';
+      if (wrongAnswers.length === 0) return 'Отличный результат!';
+      if (correctAnswers.length > wrongAnswers.length) return 'Хороший результат, продолжай учиться!';
+      return 'Продолжай учиться, в следующий раз получится лучше';
+    };
 
-  return (
-    <div className="results">
-      <h4>Твой счет : {score}</h4>
-      <h4>{getResultDesision()}</h4>
-      <div className="results_table">
+    const resultDesision = getResultDesision();
 
-        <div className={classNames('results_list', 'results_correct')}>
-          <h4>Правильные ответы:</h4>
-          {correctAnswers.map(el => (
-            <div key={el.id} className="">
-              {el.word}
-            </div>
-          ))}
-        </div>
-        <div className={classNames('results_list', 'results_wrong')}>
-          <h4>Ошибки:</h4>
-          {wrongAnswers.map(el => (
-            <div key={el.id} className="">
-              {el.word}
-            </div>
-          ))}
+    return (
+      <div className="results">
+        <h2 className='results_heading'>Твой результат : {score} баллов</h2>
+        {resultDesision && <h4>{resultDesision}</h4>}
+        <div className="results_table">
+
+          <div className={classNames('results_list', 'results_correct')}>
+            <h4 className='results_header'>Правильные ответы:</h4>
+            {correctAnswers.map((el, i) => (
+              <GameResultItem
+                item={el}
+                isOdd={i % 2 === 0}
+                key={el.id}
+              />
+            ))}
+          </div>
+          <div className={classNames('results_list', 'results_wrong')}>
+            <h4 className='results_header'>Ошибки:</h4>
+            {wrongAnswers.map((el, i) => (
+              <GameResultItem
+                item={el}
+                isOdd={i % 2 === 0}
+                key={el.id}
+              />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
 
-  );};
+    );
+  };
