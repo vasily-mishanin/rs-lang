e8867f03ff87d1312854081adf75e000747775f7
m208 <avcvet@yandex.ru>
2022-09-01 02:27:46 +0300

    feat: add base layout for game statistic

--- src/pages/Dictionary/Progress.tsx
+++ src/pages/Dictionary/Progress.tsx
@@ -4,18 +4,21 @@ import { useSelector } from 'react-redux';
 import { useEffect, useState } from 'react';
 
 import { getUserStatistic } from '@/model/api-statistic';
-import { GameStatsProgressWord } from '@/model/app-types';
+import { GameStatsProgressWord, IUserStatistic } from '@/model/app-types';
 import { RootState } from '@/store/store';
 
 const Progress = (): JSX.Element => {
 
   const [words, setWords] = useState<GameStatsProgressWord[]>([]);
+  const [stats, setStats] = useState<IUserStatistic>();
 
   const authState = useSelector((state: RootState) => state.authentication);
 
   const loadData = async () =>{
     const currentStatistic = await getUserStatistic(authState.userId, authState.token);
     if (currentStatistic) setWords(Object.values(currentStatistic?.optional?.gamesWordsProgress));
+    if (currentStatistic) setStats(currentStatistic);
+
   };
 
   useEffect(() => {
@@ -23,7 +26,27 @@ const Progress = (): JSX.Element => {
   }, [authState.userId, authState.token]);
 
   return (<section className='difficult-words w-full flex flex-col items-center'>
-    <h1>Words encountered in games</h1>
+    <h3>Game stats: </h3>
+    <p>Games played total:
+      <span>sprint : <b> {stats?.optional.gamesStatistic.gamesTotalCount.sprint} </b></span>
+      <span>audio : <b> {stats?.optional.gamesStatistic.gamesTotalCount.audio} </b></span>
+    </p>
+
+    <h3>Games per day: </h3>
+    {stats && (
+      Object.keys(stats.optional.gamesStatistic.gamesPerDay).map((el, i) => (
+        <div className="flex justify-between w-6/12"
+          key={i}
+        >
+          <p>Date: {el}</p>
+          <p>audio: {stats.optional.gamesStatistic.gamesPerDay[el].audio}</p>
+          <p>sprint: {stats.optional.gamesStatistic.gamesPerDay[el].sprint}</p>
+        </div>
+
+      ))
+    )}
+
+    <h3>Words encountered in games</h3>
     {words.map((el, i) => (
       <div className="flex justify-between w-6/12"
         key={i}
