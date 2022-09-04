6e6e2e113ea8fffd912ced085fe8aa80c60a283d
m208 <avcvet@yandex.ru>
2022-09-04 15:32:21 +0300

    feat: add navbuttons on games page

--- src/components/ui/NavButton/NavButton.tsx
+++ src/components/ui/NavButton/NavButton.tsx
@@ -1,3 +1,4 @@
+import { Link } from '@tanstack/react-location';
 import './NavButton.pcss';
 
 export interface NavButtonProps {
@@ -11,19 +12,22 @@ export interface NavButtonProps {
 }
 export const NavButton = ({ href, icon, name, description }:NavButtonProps ): JSX.Element => (
   <div className="navbutton">
-    <a
-      href={href}
-      className="navbutton_link"
-    >
-      <icon.icon className="navbutton_icon"  aria-hidden="true" />
+    <Link to={href}>
+      <div
+        //   href={href}
+        className="navbutton_link"
+      >
 
-      <div className="ml-4">
-        <p className="navbutton_title">{name}</p>
-        {description && (
-          <p className="navbutton_descr">{description}</p>
-        )}
+        <icon.icon className="navbutton_icon"  aria-hidden="true" />
+
+        <div className="ml-4">
+          <p className="navbutton_title">{name}</p>
+          {description && (
+            <p className="navbutton_descr">{description}</p>
+          )}
+        </div>
       </div>
-    </a>
+    </Link>
   </div>
 
 );
--- src/pages/Games/GamesPage.tsx
+++ src/pages/Games/GamesPage.tsx
@@ -1,18 +1,27 @@
-import { Link, Outlet } from '@tanstack/react-location';
+import { LightningBoltIcon, SpeakerphoneIcon } from '@heroicons/react/outline';
+import {  Outlet } from '@tanstack/react-location';
 
+import { NavButton } from '@/components/ui/NavButton/NavButton';
 import './GamesPage.pcss';
 
 const GamesPage = (): JSX.Element => (
   <section className="games">
-    <ul className='games_list'>
-      <li>
-        <Link to="/games/audio">Audio</Link>
-      </li>
 
-      <li>
-        <Link to="/games/sprint">Sprint</Link>
-      </li>
-    </ul>
+    <div className="flex w-full justify-center my-8">
+      <NavButton
+        href='/games/sprint'
+        icon={{ icon: LightningBoltIcon }}
+        name='Спринт'
+        description='Игра на время'
+      />
+
+      <NavButton
+        href='/games/audio'
+        icon={{ icon: SpeakerphoneIcon }}
+        name='Аудиовызов'
+        description='Игра на слух'
+      />
+    </div>
 
     <Outlet />
   </section>
